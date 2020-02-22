9e477951868cb5af79a19c3e8ede9981e0ff7c0d
Mamadou Billo Diallo <billodiallonet@gmail.com>
2020-02-22 13:54:39 +0300

    adding effet jquery on designicon image,product and project

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+            $(".clickable").click(function() {
+              $("#design-showing").toggle();
+              $("#design-hidden").toggle();
+            });
+          });
+          $(document).ready(function() {
+                      $(".clickable").click(function() {
+                        $("#dev_icon-showing").toggle();
+                        $("#dev_icon-hidden").toggle();
+                      });
+                    });
+
+                    $(document).ready(function() {
+                                $(".clickable").click(function() {
+                                  $("#product_icon-showing").toggle();
+                                  $("#product_icon-hidden").toggle();
+                                });
+                              });
