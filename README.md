885f6a10d77791b649f92e27cf0fd9b790c33e54
Mamadou Billo Diallo <billodiallonet@gmail.com>
2020-02-22 15:54:04 +0300

    roolback for index file

---
+++ README.md
@@ -0,0 +1,36 @@
+
+    # Description 
+##Authors
+- [Mamadou Billo Diallo](https://github.com/billodiallo)
+
+## Project setup instructions
+To start using this project use the following commands:
+-Create a project Folder WeekIP3
+-create js and css folders
+-make sure i have copied bootstrap in the right place js folder
+-create scripts.js file
+-write the Html code in index.html file
+-Add some code in styles.css file
+-commit all the time after change on git 
+-dowload all the image from Delani github pages and saved them in local foder
+-create in htm file classes and them ids.
+create javascript codes for clickable and hover
+
+-create the same folder weekIP3 in gitHub
+-git remote add  https://billodiallo.github.io/weekIP3/
+-git push origin master
+- `git clone https://github.com/billodiallo/Akan-names.git``
+- `atom .`
+- `code .` [//]: # (this is if Visual Studio Code is your preferred text editor)
+
+## License info
+MIT License
+
+Copyright (c) 2020 Billo DIallo
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sub license, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THESOFTWARE.
--- js/scripts.js
+++ js/scripts.js
@@ -4,16 +4,42 @@ $(document).ready(function() {
               $("#design-hidden").toggle();
             });
           });
-          $(document).ready(function() {
-                      $(".clickable").click(function() {
-                        $("#dev_icon-showing").toggle();
-                        $("#dev_icon-hidden").toggle();
+
+$(document).ready(function() {
+            $(".clickable").click(function() {
+            $("#dev_icon-showing").toggle();
+            $("#dev_icon-hidden").toggle();
                       });
                     });
 
-                    $(document).ready(function() {
-                                $(".clickable").click(function() {
-                                  $("#product_icon-showing").toggle();
-                                  $("#product_icon-hidden").toggle();
-                                });
-                              });
+  $(document).ready(function() {
+              $(".clickable").click(function() {
+              $("#product_icon-showing").toggle();
+              $("#product_icon-hidden").toggle();
+                });
+                });
+
+                $(".black").hover(function(){
+                    $(".1").slideToggle();
+                  })
+                  $(".black").hover(function(){
+                      $(".2").slideToggle();
+                    })
+                    $(".black").hover(function(){
+                        $(".3").slideToggle();
+                      })
+                    $(".black").hover(function(){
+                    $(".4").slideToggle();
+                        })
+                    $(".black").hover(function(){
+                    $(".5").slideToggle();
+                          })
+                    $(".black").hover(function(){
+                    $(".6").slideToggle();
+                            })
+                    $(".black").hover(function(){
+                      $(".7").slideToggle();
+                              })
+                      $(".black").hover(function(){
+                      $(".8").slideToggle();
+                                })
